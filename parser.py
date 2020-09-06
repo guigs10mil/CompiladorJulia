0431656b6d89dd75472a173cb351d4b4bf2fc124
guigs10mil <gui.m.s@hotmail.com>
2020-09-05 22:08:32 -0300

    Comentarios, multiplicacao e divisao

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import tokenizer
+import prepro
 
 class Parser:
     tokens = None
@@ -13,42 +14,25 @@ class Parser:
             Parser.tokens.selectNext()
 
         if (Parser.tokens.actual.type == "INT"):
-            res = int(Parser.tokens.actual.value)
-            Parser.tokens.selectNext()
+            res = Parser.parseTerm()
 
             while Parser.tokens.actual.type != "EOF":
                 if (Parser.tokens.actual.type == "PLUS"):
                     Parser.tokens.selectNext()
                     if (Parser.tokens.actual.type == "INT"):
-                        res += int(Parser.tokens.actual.value)
+                        res += Parser.parseTerm()
                     else:
                         raise ValueError("soma")
 
                 elif (Parser.tokens.actual.type == "MINUS"):
                     Parser.tokens.selectNext()
                     if (Parser.tokens.actual.type == "INT"):
-                        res -= int(Parser.tokens.actual.value)
+                        res -= Parser.parseTerm()
                     else:
                         raise ValueError("subtracao")
-
-                elif (Parser.tokens.actual.type == "MULTI"):
-                    Parser.tokens.selectNext()
-                    if (Parser.tokens.actual.type == "INT"):
-                        res *= int(Parser.tokens.actual.value)
-                    else:
-                        raise ValueError("multiplicacao")
-
-                elif (Parser.tokens.actual.type == "DIV"):
-                    Parser.tokens.selectNext()
-                    if (Parser.tokens.actual.type == "INT"):
-                        res /= int(Parser.tokens.actual.value)
-                    else:
-                        raise ValueError("divisao")
                 
                 else:
                     raise ValueError("caracter invalido depois no INT")
-
-                Parser.tokens.selectNext()
 
             return res
 
@@ -56,8 +40,37 @@ class Parser:
             raise ValueError("o primeiro nao eh int, eh " + str(Parser.tokens.actual.type))
 
     @staticmethod
+    def parseTerm():
+        # consome os tokens do Tokenizer e analisa se a sintaxe está aderente à gramática proposta. retorna o resultado da expressão analisada
+
+        res = int(Parser.tokens.actual.value)
+        Parser.tokens.selectNext()
+
+        while Parser.tokens.actual.type == "MULTI" or Parser.tokens.actual.type == "DIV":
+            if (Parser.tokens.actual.type == "MULTI"):
+                Parser.tokens.selectNext()
+                if (Parser.tokens.actual.type == "INT"):
+                    res *= int(Parser.tokens.actual.value)
+                else:
+                    raise ValueError("multiplicacao")
+
+            elif (Parser.tokens.actual.type == "DIV"):
+                Parser.tokens.selectNext()
+                if (Parser.tokens.actual.type == "INT"):
+                    res /= int(Parser.tokens.actual.value)
+                else:
+                    raise ValueError("divisao")
+            
+            else:
+                raise ValueError("caracter invalido depois no INT")
+
+            Parser.tokens.selectNext()
+
+        return res
+
+    @staticmethod
     def run(code):
         # recebe o código fonte como argumento, inicializa um objeto Tokenizador e retorna o resultado do parseExpression(). Esse método será chamado pelo main().
 
-        Parser.tokens = tokenizer.Tokenizer(code)
+        Parser.tokens = tokenizer.Tokenizer(prepro.PrePro.filter(code))
         return Parser.parseExpression()
